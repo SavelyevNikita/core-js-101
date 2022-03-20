d12fa8542f72c917a5cb69e7dd96421e82e209e6
SavelyevNikita <nik2111122@sibmail.com>
2022-03-21 01:03:09 +0300

    feat: obj

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -42,8 +42,9 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  // throw new Error('Not implemented');
+  return JSON.stringify(obj);
 }
 
 
@@ -58,8 +59,12 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  console.log(proto);
+  console.log(JSON.parse(json));
+  console.log(JSON.parse(json).radius);
+  return proto(JSON.parse(json).radius);
 }
 
 
